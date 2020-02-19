266692c2b5ad7ff682ccddca7824f58dd57cb356
piegames <git@piegames.de>
2020-02-20 00:35:53 +0100

    Fix bounds check
    This time with unit tests, so hopefully once and for all.

--- BlockMap-internal/src/test/java/de/piegames/blockmap/RegionRendererTest.java
+++ BlockMap-internal/src/test/java/de/piegames/blockmap/RegionRendererTest.java
@@ -1,5 +1,7 @@
 package de.piegames.blockmap;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
@@ -31,5 +33,31 @@ public class RegionRendererTest {
 		BufferedImage image = renderer.render(new Vector2i(0, 0), new RegionFile(Paths.get(URI.create(getClass().getResource("/r.0.0.mca").toString()))))
 				.getImage();
 		ImageIO.write(image, "png", Files.newOutputStream(folder.newFile().toPath()));
+	}
+
+	/** Test if bounds in the render settings are respected */
+	@Test
+	public void testBounds() throws IOException, URISyntaxException, InterruptedException {
+		System.setProperty("joml.format", "false");
+		RenderSettings settings = new RenderSettings();
+		settings.loadDefaultColors();
+		settings.minZ = -500;
+		settings.maxZ = -50;
+		settings.minX = 30;
+		settings.maxX = 420;
+		RegionRenderer renderer = new RegionRenderer(settings);
+		BufferedImage image = renderer.render(new Vector2i(0, -1), new RegionFile(Paths.get(URI.create(getClass().getResource("/r.1.3.mca").toString()))))
+				.getImage();
+		boolean[][] isCulled = new boolean[512][];
+		boolean[][] shouldBeCulled = new boolean[512][];
+		for (int x = 0; x < 512; x++) {
+			isCulled[x] = new boolean[512];
+			shouldBeCulled[x] = new boolean[512];
+			for (int z = 0; z < 512; z++) {
+				isCulled[x][z] = (image.getRGB(x, z) >> 24) == 0;
+				shouldBeCulled[x][z] = z < 12 || z > 462 || x < 30 || x > 420;
+			}
+		}
+		assertArrayEquals(shouldBeCulled, isCulled);
 	}
 }

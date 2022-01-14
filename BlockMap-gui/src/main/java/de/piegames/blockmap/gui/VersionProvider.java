0752702d92a93515227b32a231d797acc17a0a21
piegames <git@piegames.de>
2022-01-14 22:34:40 +0100

    Version bump: 2.4.0

--- BlockMap-gui/src/main/java/de/piegames/blockmap/gui/VersionProvider.java
+++ BlockMap-gui/src/main/java/de/piegames/blockmap/gui/VersionProvider.java
@@ -3,6 +3,6 @@ package de.piegames.blockmap.gui;
 public class VersionProvider {
 	/* This part of the code gets generated automatically through `gradle generateSources`. Do not modify! */
 	// $REPLACE_START
-	public static final String VERSION = "2.3.0";
+	public static final String VERSION = "2.4.0";
 	// $REPLACE_END
 }

d3d76c89a91e576973844f0f2d40f9f1b5590aeb
WasteLand Origin <76490476+SpiritOTHawk@users.noreply.github.com>
2022-07-22 15:43:20 +0300

    Version bump: 2.5.0

--- BlockMap-gui/src/main/java/de/piegames/blockmap/gui/VersionProvider.java
+++ BlockMap-gui/src/main/java/de/piegames/blockmap/gui/VersionProvider.java
@@ -3,6 +3,6 @@ package de.piegames.blockmap.gui;
 public class VersionProvider {
 	/* This part of the code gets generated automatically through `gradle generateSources`. Do not modify! */
 	// $REPLACE_START
-	public static final String VERSION = "2.4.1";
+	public static final String VERSION = "2.5.0";
 	// $REPLACE_END
 }
